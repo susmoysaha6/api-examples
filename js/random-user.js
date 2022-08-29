8804bc9c723ce702345d6f7650b1129e6bddff39
susmoysaha6 <susmoysaha6@gmail.com>
2022-08-29 16:41:01 +0600

    random user api

---
+++ js/random-user.js
@@ -0,0 +1,17 @@
+const loadUser = () => {
+    fetch(`https://randomuser.me/api/?results=1`)
+        .then(res => res.json())
+        .then(data => displayUser(data.results))
+}
+
+const displayUser = (users) => {
+    console.log(users);
+    const userContainer = document.getElementById('user');
+    userContainer.innerHTML = `
+    <img src="${users[0].picture.large}" alt="" />
+    <p>${users[0].name.first}</p>
+    <p><small>${users[0].location.street.name}, ${users[0].location.city}</small></p>
+
+    `
+}
+loadUser()
